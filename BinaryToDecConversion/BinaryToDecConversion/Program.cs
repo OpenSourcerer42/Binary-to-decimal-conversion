bb57afde7677f474cfdc411f7de5153ca6343085
Louis Weaver <48181533+OpenSourcerer42@users.noreply.github.com>
2019-03-03 16:52:31 +0000

    improved code, names and comments
    changed some code and some names for clear explanation as well as comments for description of parts of code

--- BinaryToDecConversion/BinaryToDecConversion/Program.cs
+++ BinaryToDecConversion/BinaryToDecConversion/Program.cs
@@ -10,18 +10,21 @@ namespace BinaryToDecConversion
     {
         static void Main(string[] args)
         {
+            //Prompts for user input to choose either decimal to binary or binary to decimal
             Console.WriteLine("Convert decimal to binary(1) or convert binary to decimal(2)");
             int ans = int.Parse(Console.ReadLine());
             int num = 0;
             int binaryNum = 0;
             if (ans == 1)
             {
+                //prompts user for a decimal value to convert
                 Console.WriteLine("enter a decimal to convert to binary");
                 num = int.Parse(Console.ReadLine());
                 DecimalConversion(num);
             }
             else if (ans == 2)
             {
+                //prompts user for a binary value to convert
                 Console.WriteLine("enter a binary value to convert to decimal");
                 binaryNum = int.Parse(Console.ReadLine());
                 BinaryConversion(binaryNum);
@@ -29,10 +32,12 @@ namespace BinaryToDecConversion
         }
         static void DecimalConversion(int dec)
         {
+
             string result = "";
             int remainder = 0;
             while (dec > 0)
             {
+                //divides decimal value by 2 until 0, stores remainder in string result and outputs to console
                 dec /= 2;
                 remainder = dec % 2;
                 result += remainder;
@@ -43,27 +48,29 @@ namespace BinaryToDecConversion
         static void BinaryConversion(int binaryNum)
         {
             double powerResult = 0;
-            string binaryArray = Convert.ToString(binaryNum);
+            string binaryString = Convert.ToString(binaryNum);
             double power = 0;
-            string[] resArray = new string[binaryArray.Length];
+            //array for all powers needed for calculation based on num of binary values
+            string[] powerArray = new string[binaryString.Length];
 
-            for (int i = 0; i < binaryArray.Length; i++)
+            for (int i = 0; i < binaryString.Length; i++)
             {
-                char c = binaryArray[i];
+                //stores each power needed in the array
                 powerResult = Math.Pow(2.0, power);
                 power += 1;
-                resArray[i] = Convert.ToString(powerResult);
+                powerArray[i] = Convert.ToString(powerResult);
             }
 
-            Array.Reverse(resArray);
+            Array.Reverse(powerArray);
+            // 16-8-4-2-0 instead of 0-2-4-8-16
             int result = 0;
             int pow = 0;
-            for (int j = 0; j < resArray.Length; j++)
+            for (int j = 0; j < powerArray.Length; j++)
             {
-                //compare the resarray to the binary values, if value = 2 multiply by power and store in integer to print
-                if (binaryArray[j] == '0')
+                //compare the array of power results to the binary values, if value = 1 add power result and store in integer to print
+                if (binaryString[j] == '1')
                 {
-                    pow = int.Parse(resArray[j]);
+                    pow = int.Parse(powerArray[j]);
                     result += pow;
                 }
             }
