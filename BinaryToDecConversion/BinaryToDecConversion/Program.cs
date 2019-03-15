3f05000a39b38cb5e728b8e01a7d7e0b10db32b4
Louis Weaver <48181533+OpenSourcerer42@users.noreply.github.com>
2019-03-15 11:32:57 +0000

    Implemented exception handling
    Exception handling has been added to ensure that the user input is of correct data type (integers) and for binary the input is only 1s and 0s

--- BinaryToDecConversion/BinaryToDecConversion/Program.cs
+++ BinaryToDecConversion/BinaryToDecConversion/Program.cs
@@ -10,24 +10,59 @@ namespace BinaryToDecConversion
     {
         static void Main(string[] args)
         {
-            //Prompts for user input to choose either decimal to binary or binary to decimal
-            Console.WriteLine("Convert decimal to binary(1) or convert binary to decimal(2)");
-            int ans = int.Parse(Console.ReadLine());
+            int ans = 0;
             int num = 0;
             int binaryNum = 0;
+            try
+            {
+                //Prompts for user input to choose either decimal to binary or binary to decimal
+                Console.WriteLine("Convert decimal to binary(1) or convert binary to decimal(2)");
+                ans = int.Parse(Console.ReadLine());
+
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Error: Please enter an Integer");
+            }
             if (ans == 1)
             {
-                //prompts user for a decimal value to convert
-                Console.WriteLine("enter a decimal to convert to binary");
-                num = int.Parse(Console.ReadLine());
-                DecimalConversion(num);
+                try
+                {
+                    //prompts user for a decimal value to convert
+                    Console.WriteLine("enter a decimal to convert to binary");
+                    num = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    DecimalConversion(num);
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Error: Please enter an Integer");
+                    Main(null);
+                }
             }
             else if (ans == 2)
             {
-                //prompts user for a binary value to convert
-                Console.WriteLine("enter a binary value to convert to decimal");
-                binaryNum = int.Parse(Console.ReadLine());
-                BinaryConversion(binaryNum);
+                try
+                {
+                    //prompts user for a binary value to convert
+                    Console.WriteLine("enter a binary value to convert to decimal");
+                    binaryNum = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    BinaryConversion(binaryNum);
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Error: Please enter an Integer");
+                    Main(null);
+                }
+            } else
+            {
+                Console.Clear();
+                Console.WriteLine("Error: please enter either 1 or 2");
+                Main(null);
             }
         }
         static void DecimalConversion(int dec)
@@ -52,7 +87,6 @@ namespace BinaryToDecConversion
             double power = 0;
             //array for all powers needed for calculation based on num of binary values
             string[] powerArray = new string[binaryString.Length];
-
             for (int i = 0; i < binaryString.Length; i++)
             {
                 //stores each power needed in the array
@@ -73,6 +107,12 @@ namespace BinaryToDecConversion
                     pow = int.Parse(powerArray[j]);
                     result += pow;
                 }
+            }
+            if (result == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Error: incorrect input, please enter a binary number consisting of only 1s and 0s");
+                Main(null);
             }
             Console.WriteLine(result);
             Console.ReadLine();
